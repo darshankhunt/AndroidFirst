4d32745f78138523b71ecfdba5d5fd7aa05d2270
darshan Khunt <darshankhunt13@gmail.com>
2023-01-24 11:29:57 +0530

    First Commit

---
+++ app/src/main/java/com/example/androidfirst/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.androidfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edUserName;
+    private Button btnSubmit;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edUserName = findViewById(R.id.edUserName);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String UserName = edUserName.getText().toString();
+                Toast.makeText(MainActivity.this,"User Name: "+UserName+" Successfully Register..!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
